d3c10abd8eac4633c29a9f6661e71901c2076c3e
Ajanthan Balachandran <balaajanthan@gmail.com>
2020-06-04 23:49:03 -0700

    setting owner

--- postgresql/postgresql-start/initdb.sh
+++ postgresql/postgresql-start/initdb.sh
@@ -1,4 +1,4 @@
 thisdir=$(dirname ${BASH_SOURCE[0]})
 init_data_file=$(readlink -f ${thisdir}/../data/init.sql)
 
-psql -d $POSTGRESQL_DATABASE -f $init_data_file
+psql -U $POSTGRESQL_USER -d $POSTGRESQL_DATABASE -f $init_data_file
